4d6e65ec4795a3913cc5a73024dc6bab672e4f48
VyrekXD <vyrekxd@gmail.com>
2020-10-19 11:30:06 -0500

    w

--- comandos/utilidad/bot-util/vote.js
+++ comandos/utilidad/bot-util/vote.js
@@ -10,10 +10,49 @@ run: async (bot, message, args) => {
     setTimeout(async () => {
 
         const e = new Discord.MessageEmbed()
-        .setDescription(`[Hype Discord List](https://hypelist.glitch.me/bots/724749468418703432/)
-        [Statcord](https://statcord.com/bot/724749468418703432)`)
-        message.channel.send(e)
+        .setDescription(`:one: [Hype Discord List](https://hypelist.glitch.me/bots/724749468418703432/)
+        :two: [Statcord](https://statcord.com/bot/724749468418703432)`)
+        let msg = await message.channel.send(e)
         
+        await msg.react('1️⃣')
+        await msg.react('2️⃣')
+        await msg.react('🗑️')
+
+        const filter = (reaction, user) => {return ['1️⃣','2️⃣','🗑️'].includes(reaction.emoji.name) && user.id === message.author.id}
+        const collector = info.createReactionCollector(filter, { time: 60000})
+
+        collector.on('collect', async reaction =>{
+            if(reaction.emoji.name === '1️⃣'){
+                info.reactions.cache.find(r => r.emoji.name == '1️⃣').users.remove(message.author.id).catch(() => {})
+
+                const e = new Discord.MessageEmbed()
+                .setAuthor(bot.user.tag, bot.user.displayAvatar)
+                .setDescription('Api In Proccess...')
+
+                msg.edit(e)
+                collector.stop()
+
+            }else if(reaction.emoji.name === '2️⃣'){
+                info.reactions.cache.find(r => r.emoji.name == '2️⃣').users.remove(message.author.id).catch(() => {})
+
+                let pet = await fetch(`https://bots.discordlabs.org/v2/bot/${bot.user.id}`, {method: 'GET'})
+                let json = pet.json()
+
+                const e = new Discord.MessageEmbed()
+                .setColor('RANDOM')
+                .setAuthor(bot.user.tag, bot.user.displayAvatar)
+                .setDescription(json.sdescription)
+                .addField(`**Votos**`, json.votes, true)
+                .addField(`**Servidores**`, json.server_count, true)
+                .addField(`**Descripcion Larga**`, json.ldescription)
+                
+                msg.edit(e)
+
+                collector.stop()
+            }
+        })
+
+        collector.on('end', () => {return msg.reactions.removeAll()})
 
         mensaje.delete()
     }, 8 * 1000);
