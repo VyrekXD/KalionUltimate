85d3259a2479175bb5c2db3844c2d90871bbe220
VyrekXD <vyrekxd@gmail.com>
2020-10-19 12:48:29 -0500

    w

--- comandos/utilidad/bot-util/help.js
+++ comandos/utilidad/bot-util/help.js
@@ -10,7 +10,7 @@ module.exports = {
       if(message.channel.nsfw){
         e.setThumbnail(bot.user.displayAvatarURL())
         .setTitle(`:page_facing_up: Comandos de el bot`)
-        .setDescription(`**Updates**\n${bot.updates[0]}`)
+        .setDescription(`**Updates**\n\`\`\`${bot.updates[0]}\`\`\``)
         .addField(`:pencil: Informacion`, `Actualmente la cantidad de comandos que tengo es: **${bot.comandos.size}**`)
         .addField(`:closed_book: Categorias:`, 
         '» `k!help economy`\n» `k!help eadmin`\n» `k!help premium`\n» `k!help miner`\n» `k!help util`\n» `k!help admin`\n» `k!help fun`\n» `k!help react`\n» `k!help music`\n» `k!help nsfw`')
@@ -20,7 +20,7 @@ module.exports = {
       }else {
         e.setThumbnail(bot.user.displayAvatarURL())
         .setTitle(`:page_facing_up: Comandos de el bot`)
-        .setDescription(`**Updates**\n${bot.updates[0]}`)
+        .setDescription(`**Updates**\n\`\`\`${bot.updates[0]}\`\`\``)
         .addField(`:pencil: Informacion`, `Actualmente la cantidad de comandos que tengo es: **${bot.comandos.size}**`)
         .addField(`:closed_book: Categorias:`, 
         '» `k!help economy`\n» `k!help eadmin`\n» `k!help premium`\n» `k!help miner`\n» `k!help util`\n» `k!help admin`\n» `k!help fun`\n» `k!help react`\n» `k!help music`')
