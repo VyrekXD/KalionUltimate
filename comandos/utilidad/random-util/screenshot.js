20e91091b338b16f1b4603aff1a670b2d18e0049
VyrekXD <vyrekxd@gmail.com>
2020-10-27 08:55:07 -0600

    w

--- comandos/utilidad/random-util/screenshot.js
+++ comandos/utilidad/random-util/screenshot.js
@@ -27,10 +27,14 @@ example: 'screenshot https://www.google.com'
 async function pup(message, url){
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const result = await checkSingleCleanURL(url)
+
   if (result && !message.channel.nsfw) return message.channel.send("Debes estar en un canal NSFW para ver cosas NSFW!");
+
   let form = await message.channel.send("Espera un momento!").catch(() => { });
   message.channel.startTyping().catch(() => { });
+
   let page;
+
   try {
     setTimeout(() => {
       message.channel.stopTyping(true);
@@ -41,9 +45,12 @@ async function pup(message, url){
       await message.channel.send(`Hubo un error abriendo la web: ${error}`).catch(() => { });
       await form.delete().catch(() => { });
     });
+    
     if (!page) return;
+
     await page.goto(url, { waitUntil: "networkidle2" });
     let screenshot = await page.screenshot({ type: "png" });
+
     if (!message.channel.nsfw) {
       const isNSFW = await check(screenshot);
       if(isNSFW) {
@@ -51,15 +58,20 @@ async function pup(message, url){
       return message.channel.send("Huuuy.... hay contenido NSFW en la imagen mejor ve esto en otro canal que sea NSFW pillin...");
       }
     }
+
     const attachment = new Discord.MessageAttachment(screenshot, "file.png");
     message.channel.stopTyping(true);
     await message.channel.send("Ping: " + (Date.now() - (form.editedTimestamp || form.createdTimestamp)) / 1000 + "s", attachment)
     await form.delete();
+
   } catch (error) {
+
     message.channel.stopTyping(true);
     await message.channel.send(`Un error ocurrio aqui lo puedes ver: ${error}`).catch(() => { });
     await form.delete().catch(() => { });
+
   } finally {
+
     try {
       if (page && page.close && page.close instanceof Function) {
         await page.close();
