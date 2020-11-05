2ccb83824db06a7ba306ddd6934b0cbab2c9a189
VyrekXD <vyrekxd@gmail.com>
2020-11-05 10:50:17 -0600

    a

--- database/models/blacklist.js
+++ database/models/blacklist.js
@@ -4,7 +4,7 @@ const schema = new mongoose.Schema({
     userID: {type: String},
     devID: {type: String},
     reason: {type: String},
-    date: {type: String}
+    date: {type: Number}
 })
 
 module.exports = mongoose.model("ublacklisted", schema);
