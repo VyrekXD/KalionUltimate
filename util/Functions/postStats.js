e9232723133fbfa3da50bef32c1de9ffea5d4360
VyrekXD <vyrekxd@gmail.com>
2020-11-10 08:28:37 -0600

    uwu

--- util/Functions/postStats.js
+++ util/Functions/postStats.js
@@ -9,7 +9,7 @@ async function postStats(bot, statcord){
 
     await fetch(`https://discordbotlist.com/api/v1/bots/${bot.user.id}/stats`, {
         method: 'POST', 
-        headers: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoxLCJpZCI6IjcyNDc0OTQ2ODQxODcwMzQzMiIsImlhdCI6MTYwNDk1NjMyMn0.-M32bEso6dtp0bZIAh_SvxG2hW--sCtZFT2PthWHWjI",
+        headers: { Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0IjoxLCJpZCI6IjcyNDc0OTQ2ODQxODcwMzQzMiIsImlhdCI6MTYwNDk1NjMyMn0.-M32bEso6dtp0bZIAh_SvxG2hW--sCtZFT2PthWHWjI"},
         body: JSON.stringify(dat)}).then(()=> {
         console.log(`Estadisticas posteadas en DiscordBotList`)
     }).catch(err => {
