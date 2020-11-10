f909a7cf5d4b1d841e96cae1bf7f76a8df109184
VyrekXD <vyrekxd@gmail.com>
2020-11-10 14:20:11 -0600

    uwu

--- comandos/utilidad/random-util/createfile.js
+++ comandos/utilidad/random-util/createfile.js
@@ -7,8 +7,9 @@ aliases: [],
 run: async (bot, message, args, send) => {
 
     if(!args[0])return send(`Ingresa texto`)
+    if(!args[1])return send(`Ingresa texto `)
 
-    let name = args.join(' ').split("--name ").slice(1).replace('.txt') || 'message'
+    let name = args.join(' ').split("--name ").slice(1) || 'message'
     let txt = args.join(' ')
     name === 'message' ? '' : txt.replace(name)
 
