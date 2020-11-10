abc7d91bbd11a8fcbcfa2c917d7108ea417d935b
VyrekXD <vyrekxd@gmail.com>
2020-11-10 12:45:50 -0600

    a

--- comandos/juegos/mineria/mine.js
+++ comandos/juegos/mineria/mine.js
@@ -24,6 +24,8 @@ run: async (bot, message, args, send) => {
         }
     }
     
+    let usuario = message.author
+
     const e = new Discord.MessageEmbed()
     .setAuthor(usuario.tag, usuario.displayAvatarURL())
     .setDescription(`Empiezas a minar...`)
