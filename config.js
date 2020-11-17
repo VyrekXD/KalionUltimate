6cea803d6b41429a487d0947f52066731872196a
VyrekXD <vyrekxd@gmail.com>
2020-11-17 09:20:54 -0600

    A

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-    token: "NzI0NzQ5NDY4NDE4NzAzNDMy.XvEtig.7fE4g3DGzQOK_oiv4Y4ZBhF4USw",
+    token: "NzI0NzQ5NDY4NDE4NzAzNDMy.XvEtig.KlNjiES62U0y1He-ydTqCUY_rbg",
     deepai: "3a5e59c1-2157-4c76-ac0c-42a4b865db7e",
     prefix: "k-",
   
@@ -9,5 +9,5 @@ let config = {
   //token pruebas: NzUwNzQzOTA1MDg3MjU4NzM4.X0--vw.iEPzrxi9swmjJuotxW9_cAt831M
 
   
-  //token normal: NzI0NzQ5NDY4NDE4NzAzNDMy.XvEtig.7fE4g3DGzQOK_oiv4Y4ZBhF4USw
+  //token normal: NzI0NzQ5NDY4NDE4NzAzNDMy.XvEtig.KlNjiES62U0y1He-ydTqCUY_rbg
   
